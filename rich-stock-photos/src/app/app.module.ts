d1666b2b155237927f967171f327225f17518c36
Richard Sithole <richardsit@gmail.com>
2019-06-30 14:09:57 +0200

    Add shared module

--- rich-stock-photos/src/app/app.module.ts
+++ rich-stock-photos/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModuleModule } from './shared-module/shared-module.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModuleModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
