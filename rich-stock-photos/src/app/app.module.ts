5287942ad4f7fee69ef0f9cc39123bf8564194e9
Richard Sithole <richardsit@gmail.com>
2019-10-28 07:42:47 +0200

    Add ng form module

--- rich-stock-photos/src/app/app.module.ts
+++ rich-stock-photos/src/app/app.module.ts
@@ -7,11 +7,14 @@ import { SharedModuleModule } from './shared-module/shared-module.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModuleModule
