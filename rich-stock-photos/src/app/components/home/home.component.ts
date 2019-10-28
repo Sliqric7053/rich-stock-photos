08489c6c965f1dc45007d3956c12f787c8112c76
Richard Sithole <richardsit@gmail.com>
2019-10-28 07:41:58 +0200

    Add email form

--- rich-stock-photos/src/app/components/home/home.component.ts
+++ rich-stock-photos/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatFormFieldAppearance } from '@angular/material';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  emailFormControl: FormControl;
+  constructor() {}
 
   ngOnInit() {
+    this.emailFormControl = new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]);
   }
 
+  scrollToElement(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
